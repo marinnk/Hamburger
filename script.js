a097aac4a3b812539dedc04e8d1c6709d0ab8904
marinnk <marinnobaka@yahoo.co.jp>
2022-03-23 21:28:32 +0900

    ナビ作成

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 $(function() {
-$(".c-menu--header").click(function() {
+$(".c-button--headermenu").click(function() {
     $(".c-close").toggleClass("open");
-    $(".p-nav").toggleClass("open")
+    $(".p-nav").toggleClass("open");
 })
 $(".c-close").click(function() {
     $(this).removeClass("open");
     $(".p-nav").removeClass("open");
 })
-
 });
