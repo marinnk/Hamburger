ef96df338914314d4e0cfbfea5041cd5a7ffe341
marinnk <marinnobaka@yahoo.co.jp>
2022-03-23 12:12:41 +0900

    ×ボタン作成

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 $(function() {
 $(".c-menu--header").click(function() {
-    $(this).toggleClass("open");
-    $(".c-close>span").toggleClass("open");
+    $(".c-close").toggleClass("open");
     $(".p-nav").toggleClass("open")
 })
-
+$(".c-close").click(function() {
+    $(this).removeClass("open");
+    $(".p-nav").removeClass("open");
+})
 
 });
