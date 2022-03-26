299dfff3ba6689cec29b8b0d63bde903876a1e97
marinnk <marinnobaka@yahoo.co.jp>
2022-03-26 11:51:10 +0900

    クラス名変更

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 $(function() {
 $(".c-button--headermenu").click(function() {
-    $(".c-close").toggleClass("open");
+    $(".c-button--close").toggleClass("open");
     $(".p-nav").toggleClass("open");
-    $(".c-close>span").toggleClass("open");
+    $(".c-button--close>span").toggleClass("open");
     $(".c-transparent-background--nav").toggleClass("open");
 })
-$(".c-close").click(function() {
-    $(".c-close").removeClass("open");
+$(".c-button--close").click(function() {
+    $(this).removeClass("open");
     $(".p-nav").removeClass("open");
     $(".c-transparent-background--nav").removeClass("open");
 })
 $(".c-transparent-background--nav").click(function() {
     $(this).removeClass("open");
     $(".p-nav").removeClass("open");
-    $(".c-close").removeClass("open");
+    $(".c-button--close").removeClass("open");
 })
 });
