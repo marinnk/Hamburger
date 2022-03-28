6cd2f68623c6696ff54bf9d0dab7326bb9e96c24
marinnk <marinnobaka@yahoo.co.jp>
2022-03-28 21:11:52 +0900

    ×ボタン作成

--- script.js
+++ script.js
@@ -14,6 +14,23 @@ $(".p-close").click(function() {
 $(".c-transparent-background--nav").click(function() {
     $(this).removeClass("open");
     $(".p-nav").removeClass("open");
-    $(".p-close__line").removeClass("open");
+    $(".p-close__line, .p-close").removeClass("open");
+})
+
+//三本線を消すため
+$(".c-button--headermenu").click(function() {
+    if($(".p-close").hasClass("open")) {
+        $(".p-close").css("display", "block");
+    }
+})
+$(".c-transparent-background--nav").click(function() {
+    if($(".p-close").hasClass("open") == false) {
+        $(".p-close").css("display", "none");
+    }
+})
+$(".p-close").click(function() {
+    if($(".p-close").hasClass("open") == false) {
+        $(".p-close").css("display", "none");
+    }
 })
 });
