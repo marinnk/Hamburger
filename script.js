7a106e350f13855d9c7a2f3914b852be295283a5
marinnk <marinnobaka@yahoo.co.jp>
2022-03-28 11:46:03 +0900

    ×ボタン作成

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ $(function() {
 $(".c-button--headermenu").click(function() {
     $(".c-button--close").toggleClass("open");
     $(".p-nav").toggleClass("open");
-    $(".c-button--close>span").toggleClass("open");
     $(".c-transparent-background--nav").toggleClass("open");
+    $(".p-close__line").toggleClass("open");
 })
 $(".c-button--close").click(function() {
     $(this).removeClass("open");
@@ -13,6 +13,7 @@ $(".c-button--close").click(function() {
 $(".c-transparent-background--nav").click(function() {
     $(this).removeClass("open");
     $(".p-nav").removeClass("open");
+    $(".p-close__line").removeClass("open");
     $(".c-button--close").removeClass("open");
 })
 });
