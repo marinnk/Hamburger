7b8d2c0b226e5ce679d2120a81154587e48e03ef
marinnk <marinnobaka@yahoo.co.jp>
2022-04-24 15:17:46 +0900

    検索フォーム作成

--- searchform.php
+++ searchform.php
@@ -1,6 +1,6 @@
 <form class="p-search">
-    <div class="p-search-form">
-        <input type="search" name="search" class="c-input-box">
+    <div class="p-search-form" method="get" action="<?php echo home_url('/'); ?>">
+        <input type="search" name="s" id="s" class="c-input-box">
     </div>
     <button type="submit" class="c-button c-button--search">検索</button>
 </form>
