2d971a0bdbd57da4f5618788a5191c9b2c324897
marinnk <marinnobaka@yahoo.co.jp>
2022-04-24 14:03:21 +0900

    サイドバーコード記述

--- front-page.php
+++ front-page.php
@@ -1,37 +1,5 @@
 <?php get_header(); ?>
-        <aside class="l-sidebar p-sidebar">
-            <button class="p-close c-button--close"><span class="p-close__line"></span></button>
-            <button type="button" class="c-button--menu c-button--sidemenu">Menu</button>
-            <nav class="p-nav">
-                    <p class="p-nav__menu"><a href="#">バーガー</a></p>
-                    <ul class="p-nav__menu-list">
-                            <li class="p-nav__menu-item"><a href="#">ハンバーガー</a></li>
-                            <li class="p-nav__menu-item"><a href="#">チーズバーガー</a></li>
-                            <li class="p-nav__menu-item"><a href="#">テリヤキバーガー</a></li>
-                            <li class="p-nav__menu-item"><a href="#">アボカドバーガー</a></li>
-                            <li class="p-nav__menu-item"><a href="#">フィッシュバーガー</a></li>
-                            <li class="p-nav__menu-item"><a href="#">ベーコンバーガー</a></li>
-                            <li><a href="#">チキンバーガー</a></li>
-                    </ul>
-                    <p class="p-nav__menu"><a href="#">サイド</a></p>
-                    <ul class="p-nav__menu-list">
-                            <li class="p-nav__menu-item"><a href="#">ポテト</a></li>
-                            <li class="p-nav__menu-item"><a href="#">サラダ</a></li>
-                            <li class="p-nav__menu-item"><a href="#">ナゲット</a></li>
-                            <li><a href="#">コーン</a></li>
-                    </ul>
-                    <p class="p-nav__menu"><a href="#">ドリンク</a></p>
-                    <ul class="p-nav__menu-list">
-                            <li class="p-nav__menu-item"><a href="#">コーラ</a></li>
-                            <li class="p-nav__menu-item"><a href="#">ファンタ</a></li>
-                            <li class="p-nav__menu-item"><a href="#">オレンジ</a></li>
-                            <li class="p-nav__menu-item"><a href="#">アップル</a></li>
-                            <li class="p-nav__menu-item"><a href="#">紅茶（Ice/Hot）</a></li>
-                            <li><a href="#">コーヒー（Ice/Hot）</a></li>
-                    </ul>
-            </nav>
-            <div class="c-transparent-background--nav"></div><!-- 透明背景 -->
-        </aside>
+<?php get_sidebar(); ?>
     <main class="l-main">
         <div class="c-mainvisual c-mainvisual--front-page">
             <h2 class="c-title--mainvisual c-title--front-page">ダミーサイト</h2>
