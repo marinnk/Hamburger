7834619d0c8e46d94a686944028ec17dd2f57644
marinnk <marinnobaka@yahoo.co.jp>
2022-04-24 15:14:24 +0900

    検索フォーム作成

---
+++ searchform.php
@@ -0,0 +1,6 @@
+<form class="p-search">
+    <div class="p-search-form">
+        <input type="search" name="search" class="c-input-box">
+    </div>
+    <button type="submit" class="c-button c-button--search">検索</button>
+</form>
