5f135b1738de6fccd3eb06ec87e617d7d2a6634e
marinnk <marinnobaka@yahoo.co.jp>
2022-04-24 14:00:42 +0900

    フッターコード記述

---
+++ footer.php
@@ -0,0 +1,15 @@
+<footer class="l-footer">
+        <div class="p-footer">
+            <ul class="p-footer__list">
+                <li class="p-footer__border"><a href="#" class="p-footer__list-item">ショップ情報</a></li>
+                <li><a href="#" class="p-footer__list-item">ヒストリー</a></li>
+            </ul>
+            <p class="p-footer__copyright">Copyright: RaiseTech</p>
+        </div>
+    </footer>
+</div> 
+<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+<script src="script.js"></script>
+<?php wp_footer(); ?>
+</body>
+</html>
