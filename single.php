62d5334095b6a89788f7578e0b07bd9c19872e88
marinnk <marinnobaka@yahoo.co.jp>
2022-04-26 11:10:58 +0900

    投稿ページ作成

--- single.php
+++ single.php
@@ -1,19 +1,24 @@
 <?php get_header(); ?>
 <?php get_sidebar(); ?>
                 <main class="l-main">
+                <?php
+                    if( have_posts() ) : //1.投稿データがあるかの条件分岐。
+                        while( have_posts() ) : // 2.表示する投稿データがあれば繰り返し処理開始
+                            the_post();
+                ?>
                         <div class="c-mainvisual c-mainvisual--single">
-                            <h2 class="c-title--mainvisual c-title--single">h1 チーズバーガー</h2>
+                            <h2 class="c-title--mainvisual c-title--single"><?php the_title(); ?></h2>
                         </div>
-                    <div class="p-main">  
+                    <div class="p-main">
                         <section>
                             <article>
-                                <h2 class="c-title--heading">見出しh2</h2>
-                                <p>pタグテキスト。pタグテキスト。pタグテキスト。pタグテキスト。pタグテキスト。pタグテキスト。pタグテキスト。pタグテキスト。pタグテキスト。pタグテキスト。pタグテキスト。pタグテキスト。pタグテキスト。pタグテキスト。pタグテキスト。</p>
+                                <h2 class="c-title--heading"><?php the_title(); ?></h2>
+                                <p><?php the_content(); ?></p>
                             </article>
-                            <h3>見出しh3</h3>
-                            <h4>見出しh4</h4>
-                            <h5>見出しh5</h5>
-                            <h6>見出しh6</h6>
+                            <h3><?php the_title(); ?></h3>
+                            <h4><?php the_title(); ?></h4>
+                            <h5><?php the_title(); ?></h5>
+                            <h6><?php the_title(); ?></h6>
                         </section>
                         <div class="p-quote">
                             <p class="p-quote__text">引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ</p>
@@ -23,11 +28,11 @@
                             <p><img src="img/hamburger.jpg" alt="ハンバーガーの写真"></p>
                             <div class="p-item">
                                 <p class="p-item__photo"><img src="img/hamburger2.jpg" alt="ハンバーガーの写真"></p>
-                                <p class="p-item__text">テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります </p>
+                                <p class="p-item__text"><?php the_content(); ?></p>
                             </div>
                             <div class="p-item">
                                 <p class="p-item__photo"><img src="img/hamburger2.jpg" alt="ハンバーガーの写真"></p>
-                                <p class="p-item__text">テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります </p>
+                                <p class="p-item__text"><?php the_content(); ?></p>
                             </div>
                             <p class="p-gallery__photo"><img src="img/hamburger2.jpg" alt="ハンバーガーの写真"></p>
                             <div class="p-gallery__photo-list">
@@ -104,5 +109,10 @@
                         <button class="c-button c-button--main">ボタン</button>
                         <p class="p-main__text--page">boldboldboldboldboldboldboldboldbold</p>
                     </div>
+                    endwhile; // 5.繰り返し処理ここまで。投稿データがまだあればwhileに戻る。なければ終了
+                        else : //6.投稿データがなければ
+                            ?><p>表示する記事がありません</p><?php //7.ない時の処理
+                        endif; ?> 
+                ?>  
                 </main>
                 <?php get_footer(); ?>
