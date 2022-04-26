f309e9d6f667b2c2329cc4805ededf96e85f6083
marinnk <marinnobaka@yahoo.co.jp>
2022-04-26 10:42:19 +0900

    インデント調節

--- sidebar.php
+++ sidebar.php
@@ -1,7 +1,7 @@
 <aside class="l-sidebar p-sidebar">
-            <button class="p-close c-button--close"><span class="p-close__line"></span></button>
-            <button type="button" class="c-button--menu c-button--sidemenu">Menu</button>
-            <?php wp_nav_menu(); ?>
-            <div class="c-transparent-background--nav"></div><!-- 透明背景 -->
-            <?php wp_sidebar(); ?>
-        </aside>
+    <button class="p-close c-button--close"><span class="p-close__line"></span></button>
+    <button type="button" class="c-button--menu c-button--sidemenu">Menu</button>
+    <?php wp_nav_menu(); ?>
+    <div class="c-transparent-background--nav"></div><!-- 透明背景 -->
+    <?php wp_sidebar(); ?>
+</aside>
