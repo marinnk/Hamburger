5abac5906fbeca0b3344b5aa6a996ab33de590c3
marinnk <marinnobaka@yahoo.co.jp>
2022-04-27 11:15:41 +0900

    header.phpの作成

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ハンバーガーサイト</title>
+    <title><?php bloginfo( 'name' ); ?>ハンバーガーサイト</title>
     <!-- fontawesome読み込み -->
     <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
     <?php wp_head(); ?>
@@ -13,7 +13,7 @@
     <div class="c-grid"> 
         <header class="l-header">
             <div class="p-header">
-                <h1 class="c-header__logo">Hamburger</h1>
+                <h1 class="c-header__logo"><a href="<?php echo esc_url( home_url( '/' ) ); ?>">Hamburger</a></h1>
                 <form class="p-search">
                     <div class="p-search-form">
                         <input type="search" name="search" class="c-input-box">
