efe6a23b89f1be34e6623c8634cda5f5df73c3d2
marinnk <marinnobaka@yahoo.co.jp>
2022-04-28 11:10:21 +0900

    サイト内リンク作成

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
         <div class="p-footer">
             <ul class="p-footer__list">
                 <li class="p-footer__border"><a href="<?php echo esc_url( home_url( '/page' ) );  ?>" class="p-footer__list-item">ショップ情報</a></li>
-                <li><a href="#" class="p-footer__list-item">ヒストリー</a></li>
+                <li><a href="<?php echo esc_url( home_url( '/history' ) );  ?>" class="p-footer__list-item">ヒストリー</a></li>
             </ul>
             <p class="p-footer__copyright">Copyright: RaiseTech</p>
         </div>
