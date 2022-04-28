e8adf0080e56ed6aa38be9def688bd8e031b8e58
marinnk <marinnobaka@yahoo.co.jp>
2022-04-28 12:15:20 +0900

    記述

--- functions.php
+++ functions.php
@@ -1,8 +1,15 @@
 <?php 
+
+    function my_setup() {
+      add_theme_support('post-thumbnails');
+      add_theme_support( 'title-tag' );
+      add_theme_support( 'automatic-feed-links' );
+    }
+
     //cssとjQueryの読み込み
     function my_scripts() {
-        wp_enqueue_style( 'style-name', get_template_directory_uri() . '/css/style.css', array(), '1.0.0', 'all' );
-        wp_enqueue_script( 'script-name', get_template_directory_uri() . '/script.js', array( 'jquery' ), '1.0.0', true );
+      wp_enqueue_style( 'style-name', get_template_directory_uri() . '/css/style.css', array(), '1.0.0', 'all' );
+      wp_enqueue_script( 'script-name', get_template_directory_uri() . '/script.js', array( 'jquery' ), '1.0.0', true );
     }
     add_action( 'wp_enqueue_scripts', 'my_scripts' );
 
