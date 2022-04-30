3f965403e3b1c934227b5c4fd8cad125481552af
marinnk <marinnobaka@yahoo.co.jp>
2022-04-30 17:12:44 +0900

    ナビ作成

--- sidebar.php
+++ sidebar.php
@@ -1,6 +1,8 @@
 <aside class="l-sidebar p-sidebar">
             <button class="p-close c-button--close"><span class="p-close__line"></span></button>
             <button type="button" class="c-button--menu c-button--sidemenu">Menu</button>
-            <?php wp_nav_menu(); ?>
+            <?php wp_nav_menu( array(
+                    'nav' => 'サイドバー',
+            ) ); ?>
             <div class="c-transparent-background--nav"></div><!-- 透明背景 -->
         </aside>
