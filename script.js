30240892acad5c35d2c1bc89158fbd728cec595e
marinnk <marinnobaka@yahoo.co.jp>
2022-05-02 11:32:32 +0900

    ナビ作成

--- script.js
+++ script.js
@@ -2,20 +2,20 @@ $(function() {
 $(".c-button--headermenu").click(function() { //menuボタンをクリックしたらopenをつける
     $(this).toggleClass("open");
     $(".c-button--close").toggleClass("open");
-    $(".menu-categorymenu-container").toggleClass("open");
+    $(".p-nav").toggleClass("open");
     $(".c-transparent-background--nav").toggleClass("open");
     $(".p-close__line").toggleClass("open");
 })
 $(".p-close").click(function() { //×ボタンをクリックしたらopenが消える
     $(this).removeClass("open");
-    $(".menu-categorymenu-container").removeClass("open");
+    $(".p-nav").removeClass("open");
     $(".p-close__line").removeClass("open");
     $(".c-transparent-background--nav").removeClass("open");
     $(".c-button--headermenu").removeClass("open");
 })
 $(".c-transparent-background--nav").click(function() { //透明背景をクリックしてもopenが消える
     $(this).removeClass("open");
-    $(".menu-categorymenu-container").removeClass("open");
+    $(".p-nav").removeClass("open");
     $(".p-close__line, .p-close").removeClass("open");
     $(".c-button--headermenu").removeClass("open");
 })
--- sidebar.php
+++ sidebar.php
@@ -2,7 +2,10 @@
             <button class="p-close c-button--close"><span class="p-close__line"></span></button>
             <button type="button" class="c-button--menu c-button--sidemenu">Menu</button>
             <?php wp_nav_menu( array(
-                    'theme_location' => 'cateogrymenu',
+                    'menu' => 'categorymenu',
+                    'container' => 'nav',
+                    'container_class' => 'p-nav',
+                    'theme_location' => 'categorymenu'
             ) ); ?>
             <div class="c-transparent-background--nav"></div><!-- 透明背景 -->
         </aside>
