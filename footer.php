2162d92470dbf01773c51427845a14a884e2c214
marinnk <marinnobaka@yahoo.co.jp>
2022-05-03 10:21:59 +0900

    フッターナビ作成

--- footer.php
+++ footer.php
@@ -1,9 +1,14 @@
 <footer class="l-footer">
         <div class="p-footer">
-            <ul class="p-footer__list">
-                <li class="p-footer__border"><a href="<?php echo esc_url( home_url( '/page' ) );  ?>" class="p-footer__list-item">ショップ情報</a></li>
-                <li><a href="<?php echo esc_url( home_url( '/history' ) );  ?>" class="p-footer__list-item">ヒストリー</a></li>
-            </ul>
+            <?php wp_nav_menu( array(
+                'theme_location' => 'footer-menu',
+                'container_class' => 'p-footer__list'
+            ));
+            ?>
+            <!-- <ul class="p-footer__list">
+                <li class="p-footer__border"><a href="#" class="p-footer__list-item">ショップ情報</a></li>
+                <li><a href="#" class="p-footer__list-item">ヒストリー</a></li>
+            </ul> -->
             <p class="p-footer__copyright">Copyright: RaiseTech</p>
         </div>
     </footer>
