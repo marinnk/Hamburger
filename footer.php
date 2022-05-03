40eb859b52a717b1770d898f8425365b2db8ef47
marinnk <marinnobaka@yahoo.co.jp>
2022-05-03 10:30:27 +0900

    フッター元のナビ削除

--- footer.php
+++ footer.php
@@ -5,10 +5,6 @@
                 'container_class' => 'p-footer__list'
             ));
             ?>
-            <!-- <ul class="p-footer__list">
-                <li class="p-footer__border"><a href="#" class="p-footer__list-item">ショップ情報</a></li>
-                <li><a href="#" class="p-footer__list-item">ヒストリー</a></li>
-            </ul> -->
             <p class="p-footer__copyright">Copyright: RaiseTech</p>
         </div>
     </footer>
