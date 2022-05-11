1ab43cea49b3514db57045082d8f3d2b2cecc506
marinnk <marinnobaka@yahoo.co.jp>
2022-05-11 11:07:19 +0900

    画像パス変更

--- archive.php
+++ archive.php
@@ -4,7 +4,7 @@
             <div class="c-mainvisual c-mainvisual--archive p-group--mainvisual">
                 <div class="c-transparent-background--archive"></div>
                 <h2 class="c-title--mainvisual c-title--mainvisual--archive">Menu:</h2>
-                <p class="c-text--mainvisual"><?php the_category(); ?></p>
+                <p class="c-text--mainvisual"><?php the_category(); ?>
             </div>
             <div class="p-main">
                 <article>
@@ -14,7 +14,7 @@
                 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
                 <article>
                     <figure class="p-menu">
-                        <p class="p-menu__photo" ><img src="img/hamburger3.jpg" alt="ハンバーガーの写真"></p>
+                        <p class="p-menu__photo" ><img src="<?php echo esc_url(get_theme_file_uri('img/hamburger3.jpg')); ?>" alt="ハンバーガーの写真"></p>
                         <figcaption class="p-menu__text-box">
                             <h3 class="p-menu__title"><?php the_title(); ?></h3>
                             <h4 class="p-menu__heading">小見出しが入ります</h4>
@@ -44,7 +44,7 @@
                 <?php endwhile; endif; ?>
                 <ul class="p-pagination">
                     <li class="p-pagination__page-number">page 1/10</li>
-                    <li class="p-pagination__mark--front"><a href="#"><img class="p-pagination__mark p-pagination__mark--tab" src="img/before.svg" alt="前へ"></a></li>
+                    <li class="p-pagination__mark--front"><a href="#"><img class="p-pagination__mark p-pagination__mark--tab" src="<?php echo esc_url(get_theme_file_uri('img/before.svg')); ?>" alt="前へ"></a></li>
                     <li><a href="#" class="p-pagination__button--front">前へ</a></li>
                     <li><a href="#" class="p-pagination__number">1</a></li>
                     <li><a href="#" class="p-pagination__number">2</a></li>
@@ -56,7 +56,7 @@
                     <li><a href="#" class="p-pagination__number">8</a></li>
                     <li><a href="#" class="p-pagination__number">9</a></li>
                     <li><a href="#" class="p-pagination__button--next">次へ</a></li>
-                    <li class="p-pagination__mark--next"><a href="#"><img class="p-pagination__mark p-pagination__mark--tab" src="img/next.svg" alt="次へ"></a></li>
+                    <li class="p-pagination__mark--next"><a href="#"><img class="p-pagination__mark p-pagination__mark--tab" src="<?php echo esc_url(get_theme_file_uri('img/next.svg')); ?>" alt="次へ"></a></li>
                 </ul>
             </div>
         </main>
