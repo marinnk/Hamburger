0eefb45d574b71908b4f83eecfaa5c8dfdd2a650
marinnk <marinnobaka@yahoo.co.jp>
2022-05-29 12:56:48 +0900

    検索ページ作成

--- search.php
+++ search.php
@@ -1,18 +1,14 @@
-<?php if (have_posts()): ?>
-<?php
-  if (isset($_GET['s']) && empty($_GET['s'])) {
-    echo '検索キーワード未入力'; // 検索キーワードが未入力の場合のテキストを指定
-  } else {
-    echo '“'.$_GET['s'] .'”の検索結果：'.$wp_query->found_posts .'件'; // 検索キーワードと該当件数を表示
-  }
-?>
-<ul>
-<?php while(have_posts()): the_post(); ?>
-<li>
-<a href="<?php the_permalink(); ?>"><?php echo get_the_title(); ?></a>
-</li>
-<?php endwhile; ?>
-</ul>
-<?php else: ?>
-検索されたキーワードにマッチする記事はありませんでした
-<?php endif; ?>
+<?php get_header(); ?>
+
+<h2>検索結果</h2>
+<p>「<?php echo the_search_query() ?>」の検索結果は<?php echo $wp_query->found_posts; ?>件です。</p>
+<!-- 検索結果の記事表示 -->
+<?php if(have_posts()): while(have_posts()): the_post(); ?>
+
+<?php endwhile; else: ?>
+<p>検索結果は見つかりませんでした。</p>
+<?php endif; ?>  
+
+<!-- ページネーション -->
+
+<?php get_footer(); ?>
