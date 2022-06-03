8ad677c71fdca75b0e4f6458e9821444d808c347
marinnk <marinnobaka@yahoo.co.jp>
2022-06-03 11:58:12 +0900

    titleタグ削除

--- header.php
+++ header.php
@@ -4,7 +4,6 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php bloginfo('name'); ?></title>
     <!-- fontawesome読み込み -->
     <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
     <?php wp_head(); ?>
